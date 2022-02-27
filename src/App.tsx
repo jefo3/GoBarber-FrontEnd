715467aec37faf0cb62056813cfbacadb1ac8744
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2022-02-27 17:59:49 -0300

    chure: colocando a pagina de longIN no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-// import SingIN from './pages/SingIN';
+import SingIN from './pages/SingIN';
 import SingUP from './pages/SingUP';
 
 const App: React.FC = () => {
   return (
     <>
-      <SingUP />
+      <SingIN />
       <GlobalStyle />
     </>
   );
